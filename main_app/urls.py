514af91e6e3f85d28e4de2309f29c11f56ef39f1
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-12 13:43:57 -0600

    add cat index

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # second arg - view to render
     # third arg - names the route
     path('', views.home, name='home'),
-    path('about/', views.about, name="about"),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+cats = [
+    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+    {'name': 'Chunky Monkey', 'breed': 'long hair', 'description': 'sweet and cuddly', 'age': 0},
+]
 
 # Define the home view - '/'
 # GET - Home
@@ -11,3 +15,8 @@ def home(request):
 # Define the about view - about/
 def about(request):
     return render(request, 'about.html')
+
+# Index View - shows all cats at '/cats'
+def cats_index(request):
+    # just like in EJS we can pass data to views
+    return render(request, 'cats/index.html', { 'cats': cats })
