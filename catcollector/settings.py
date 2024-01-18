e742258c2234a67e8c394513c26e36ee5dfcc1f3
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-18 13:21:26 -0600

    add logout functionality

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Successful logins should redirect to cats
+# Successful logins/logouts should redirect to cats
 LOGIN_REDIRECT_URL = '/cats/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
